4f32f365c6b68f235d379a377b338d68f465ef17
granpepito <alexandre.goncalvesm@gmail.com>
2019-08-31 02:44:01 +0200

    database connection

---
+++ models/database.js
@@ -0,0 +1,31 @@
+const pool = require('mysql2');
+
+let pool = mysql.createPool({
+	host: process.env.DB_HOST || 'localhost',
+	user: process.env.DB_USER || 'root',
+	password: process.env.DB_PASSWORD || '',
+	database: process.env.DB_DATABASE || 'prpose',
+	dateStrings: true,
+	supportBigNumbers: true
+});
+
+pool.getConnection((err, connection) => {
+	if (err) {
+		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+			console.error('Database connection was closed.');
+		}
+		if (err.code === 'ER_CON_COUNT_ERROR') {
+			console.error('Database has too many connections.');
+		}
+		if (err.code === 'ECONNREFUSED') {
+			console.error('Database connection was refused.');
+		}
+	}
+
+	if (connection) {
+		console.log('Database connection has been accepted');
+		connection.release();
+	}
+});
+
+module.exports = pool;
