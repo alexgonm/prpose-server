cc38a99f9eddb37dee70c691507545dcb2b5fccf
granpepito <alexandre.goncalvesm@gmail.com>
2019-09-23 00:15:21 +0200

    users' routes and controllers started

---
+++ controllers/users.js
@@ -0,0 +1,113 @@
+const db = require('../models/database').promise();
+
+const User = {
+	findAll(queryUrl, cb) {
+		if (Object.keys(queryUrl).length === 0) {
+			queryUrl.offset = 0;
+			queryUrl.limit = 25;
+		}
+
+		const query = 'SELECT user_id, username FROM users LIMIT ?, ?';
+
+		db.execute(query, [queryUrl.offset, queryUrl.limit])
+			.then((rows, fields) => {
+				if (rows[0].length === 0) {
+					return cb(null, false);
+				}
+				cb(null, rows[0]);
+			})
+			.catch(error => {
+				return cb(error);
+			});
+	},
+	// findSome(username, cb) {
+	// 	const query = '';
+	// },
+	findOne({ params, session }, cb) {
+		const query = session.username
+			? 'SELECT user_id, username, email, creation_date FROM users WHERE username = ?;'
+			: 'SELECT user_id, username, creation_date FROM users WHERE username = ?;';
+
+		const username = params.username;
+
+		db.execute(query, [username])
+			.then((rows, fields) => {
+				if (rows[0].length === 0) {
+					return cb(null, false);
+				}
+				cb(null, rows[0]);
+			})
+			.catch(error => {
+				return cb(error);
+			});
+	},
+
+	findPosts({ params, queryUrl }, cb) {
+		//TODO: add options from req.query
+		//TODO: add the order types (best, top, new)
+		const query =
+			'SELECT posts.* FROM posts, users WHERE users.username = posts.username AND users.username = ?';
+
+		const username = params.username;
+
+		db.execute(query, [username])
+			.then((rows, fields) => {
+				if (rows[0].length === 0) {
+					return cb(null, false);
+				}
+				cb(null, rows[0]);
+			})
+			.catch(error => {
+				return cb(error);
+			});
+	},
+	findComments({ params, queryUrl }, cb) {
+		if (Object.keys(queryUrl).length === 0) {
+			queryUrl.offset = 0;
+			queryUrl.limit = 25;
+			queryUrl.order = 'desc';
+		}
+
+		//TODO: use the options from queryUrl, add the order types (best, top, new)
+		const query =
+			'SELECT comments.* FROM comments, users WHERE users.username = comments.username AND users.username = ?';
+		const username = params.username;
+
+		db.execute(query, [username])
+			.then(rows => {
+				if (rows[0].length === 0) {
+					return cb(null, false);
+				}
+				cb(null, rows[0]);
+			})
+			.catch(error => {
+				return cb(error);
+			});
+	},
+	findVotes({ queryUrl, session }, cb) {
+		if (Object.keys(queryUrl).length === 0) {
+			queryUrl.offset = 0;
+			queryUrl.limit = 25;
+			queryUrl.order = 'desc';
+		}
+
+		//TODO: finish this query
+		const query = 'SELECT FROM WHERE ';
+
+		db.execute(query, [
+			session.username,
+			queryUrl.order,
+			queryUrl.offset,
+			queryUrl.limit
+		])
+			.then(rows => {
+				if (rows[0].length === 0) {
+					return cb(null, false);
+				}
+				cb(null, rows[0]);
+			})
+			.catch(error => cb(error));
+	}
+};
+
+module.exports = User;
---
+++ routes/users.js
@@ -0,0 +1,68 @@
+const User = require('../controllers/users'),
+	router = require('express').Router();
+
+router
+	.get('/all', (req, res) => {
+		const { query } = req;
+		User.findAll(query, (err, results) => {
+			if (err) {
+				res.status(500).send(err);
+			} //else if (!results) {
+			// 	res.sendStatus(404);
+			// }
+			res.send(results);
+		});
+	})
+	.get('/:username', (req, res) => {
+		const { params, session } = req;
+
+		User.findOne({ params, session }, (err, results) => {
+			if (err) {
+				res.status(500).send(err);
+			} else if (!results) {
+				res.sendStatus(404);
+			}
+			res.send(results);
+		});
+	})
+	.get('/:username/posts', (req, res) => {
+		const { params, query } = req;
+		User.findPosts({ params, query }, (err, results) => {
+			if (err) {
+				res.status(500).send(err);
+			} else if (!results) {
+				res.sendStatus(404);
+			}
+			res.send(results);
+		});
+	})
+	.get('/:username/comments', (req, res) => {
+		const { params, query } = req;
+		User.findComments({ params, query }, (err, results) => {
+			if (err) {
+				res.status(500).send(err);
+			} else if (!results) {
+				res.sendStatus(404);
+			}
+			res.send(results);
+		});
+	})
+	.get('/:username/votes', (req, res) => {
+		if (!req.session.username) {
+			res.sendStatus(401);
+		}
+		const { session, query } = req;
+
+		User.findVotes({ query, session }, (err, results) => {
+			if (err) {
+				res.status(500).send(err);
+			}
+			if (!results) {
+				res.sendStatus(404);
+			} else {
+				res.send(results);
+			}
+		});
+	});
+
+module.exports = router;
